f002ee5af8c012af55fdc234f0a4075b4fa15e9a
Kayne Ruse <kayneruse@gmail.com>
2015-06-25 02:03:45 +1000

    Playing around with the JSON tool, tweaked the tool's output
    I think I have a handle on this. I've modified the default output for the tool
    to remove the quotation marks that were automatically put around the string
    entries. This makes the console output much cleaner.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Move along, nothing to see here.
 
 ## Credits
 
-Using and including nlohmann's json tool ([x](https://github.com/nlohmann/json))
+Using and including nlohmann's json tool ([x](https://github.com/nlohmann/json))  
+Using the MtG json database available here: [mtgjson.com](http://mtgjson.com/)  
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
-#include "value.hpp"
+#include "json.hpp"
 
+#include <iomanip>
 #include <iostream>
+#include <fstream>
 
 int main(int, char**) {
-	std::cout << "Hello world" << std::endl;
+	//load the json file
+	std::ifstream is("../rsc/AllSets.json");
+
+	if (!is.is_open()) {
+		std::cerr << "Failed to open the json file" << std::endl;
+		return 1;
+	}
+
+	//load the json file
+	nlohmann::json j;
+	j << is;
+	is.close();
+
+	//testing
+	for (auto it : j["KTK"]["cards"]) {
+		//the json internals require the call to "dump()" to retrieve a plain string
+		std::cout << std::setw(30) << std::left << it["name"].dump();
+
+		//only print the mana cost if it's available
+		if (it["manaCost"].type() != nlohmann::json::value_t::null) {
+			std::cout << std::setw(30) << std::left << it["manaCost"].dump();
+		}
+
+		//clear output
+		std::cout << std::endl;
+	}
+
+	//done
 	return 0;
 }
